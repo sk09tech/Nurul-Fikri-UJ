e629ad0cef80f36c42efc53614ab52d310dd7f62
Dayu Hermawan <102023071+sk09tech@users.noreply.github.com>
2025-10-25 16:19:50 +0700

    Create Navbar.tsx

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav style={{
+      background: "#0056A6",
+      padding: "12px",
+      color: "white",
+      fontWeight: "bold"
+    }}>
+      Dashboard PM Kerjasama
+    </nav>
+  );
+}
