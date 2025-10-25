bf77a293373885c365416088ee82db41bfd0e1f8
Dayu Hermawan <102023071+sk09tech@users.noreply.github.com>
2025-10-25 16:44:29 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,13 @@
-
-'use client'
-import { useRouter } from 'next/navigation';
 export default function Home() {
-  const router = useRouter();
   return (
     <div style={{
-      height: '100vh',
-      display:'flex',
-      justifyContent:'center',
-      alignItems:'center',
-      flexDirection:'column',
-      background:'#0A0F29',
-      color:'white'
+      height: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontSize: "30px",
     }}>
-      <h1 style={{fontSize:28, marginBottom:12}}>DASHBOARD PM KERJASAMA</h1>
-      <button
-        style={{
-          marginTop:20,
-          padding:'12px 24px',
-          background:'#0070f3',
-          borderRadius:8,
-          border:'none',
-          fontSize:16,
-          cursor:'pointer',
-          color:'white'
-        }}
-        onClick={() => router.push('/dashboard')}
-      >
-        Masuk Dashboard
-      </button>
+      Selamat Datang!
     </div>
   );
 }
